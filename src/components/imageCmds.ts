fc85f3fdd8bb075372ef76e35531fe3ac973b22d
keXXpert <kexxpert@ya.ru>
2021-08-29 13:56:10 +0300

    updated utils and commands, fixed issues

--- src/components/imageCmds.ts
+++ src/components/imageCmds.ts
@@ -1,16 +1,18 @@
 import {spawn} from 'child_process';
 
 const encoding = 'utf8';
+const identifyPath = process.env.IM_IDENTIFY || 'identify';
+const convertPath = process.env.IM_CONVERT || 'convert';
 
 export async function identifyIm(args: string[]) {
-	return spawnProcess('identify', args);
+	return spawnProcess(identifyPath, args);
 }
 
 export async function convertIm(args: string[]) {
-	return spawnProcess('convert', args);
+	return spawnProcess(convertPath, args);
 }
 
-function spawnProcess(process: string, args: string[]) {
+function spawnProcess(process: string, args: string[]): Promise<IResponse> {
 	return new Promise((resolve, reject) => {
 		const result = spawn(process, args, {});
 		let errorAcc: string | null = null;
@@ -28,14 +30,15 @@ function spawnProcess(process: string, args: string[]) {
 		});
 
 		result.on('close', (exit_code) => {
-			if (errorAcc) {
+			if (exit_code !== 0) {
 				reject({
 					exit_code,
 					process,
-					message: errorAcc || 'Unknown error'
+					stderr: errorAcc || 'Unknown error'
 				});
 			} else {
 				resolve({
+					stderr: errorAcc,
 					exit_code,
 					process,
 					data: dataAcc
@@ -45,3 +48,9 @@ function spawnProcess(process: string, args: string[]) {
 	});
 }
 
+interface IResponse {
+	exit_code: number;
+	process: string;
+	stderr?: string | null;
+	data?: string;
+}
